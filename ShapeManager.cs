c732f9f89f98ff4042fa3e1dcdd4ac6370c10195
Ahmed Alzubaidi <Ahmed.Alzubaidi@tuwaiq.edu.sa>
2021-04-28 03:20:01 +0300

    refactor: less code FTW

--- ShapeManager.cs
+++ ShapeManager.cs
@@ -11,7 +11,7 @@ namespace LimitlessDrawEngine
 {
     public class ShapeManager
     {
-        private static readonly byte[] Magic_Number = { 26, 26, 26 };
+        private static readonly byte[] Magic_Number = { 200, 30, 6 };
         private static readonly byte[] Version = { 0, 1, 0 };
 
         // close your eyes before running
@@ -38,46 +38,28 @@ namespace LimitlessDrawEngine
 
                 foreach(var shape in shapes)
                 {
-                    if(shape.GetType() == typeof(Line))
+                    if (shape.GetType() == typeof(Line))
                     {
-                        Line line = (Line)shape;
                         writer.Write("line");
-                        writer.Write(line.pointA.X);
-                        writer.Write(line.pointA.Y);
-
-                        writer.Write(line.pointB.X);
-                        writer.Write(line.pointB.Y);
-
-                        writer.Write(line.Pen.Color.Name);
-                        writer.Write((int)line.Pen.DashStyle);
-                        writer.Write(line.Pen.Width);
                     }
-                    else if(shape.GetType() == typeof(Rectangle))
+                    else if (shape.GetType() == typeof(Rectangle))
                     {
-                        Rectangle rectangle = (Rectangle)shape;
                         writer.Write("rectangle");
-                        writer.Write(rectangle.pointA.X);
-                        writer.Write(rectangle.pointA.Y);
-
-                        writer.Write(rectangle.pointB.X);
-                        writer.Write(rectangle.pointB.Y);
-
-                        writer.Write(rectangle.Pen.Color.Name);
-                        writer.Write((int)rectangle.Pen.DashStyle);
                     }
                     else if (shape.GetType() == typeof(Circle))
                     {
-                        Circle circle = (Circle)shape;
                         writer.Write("circle");
-                        writer.Write(circle.pointA.X);
-                        writer.Write(circle.pointA.Y);
-
-                        writer.Write(circle.pointB.X);
-                        writer.Write(circle.pointB.Y);
-
-                        writer.Write(circle.Pen.Color.Name);
-                        writer.Write((int)circle.Pen.DashStyle);
                     }
+
+                    writer.Write(shape.pointA.X);
+                    writer.Write(shape.pointA.Y);
+
+                    writer.Write(shape.pointB.X);
+                    writer.Write(shape.pointB.Y);
+
+                    writer.Write(shape.Pen.Color.Name);
+                    writer.Write((int)shape.Pen.DashStyle);
+                    writer.Write(shape.Pen.Width);
                 }
 
                 writer.Close();
@@ -95,7 +77,7 @@ namespace LimitlessDrawEngine
 
                 using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
                 {
-                    if(reader.BaseStream.Length < 10)
+                    if(reader.BaseStream.Length < 7)
                     {
                         reader.Close();
                         throw new Exception("Error wrong file format");
