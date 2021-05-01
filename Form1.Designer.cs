ca9407f446fee24da0a0050aaf5ef38f7c113b6c
1riyad <riyad.alghamdi@tuwaiq.edu.sa>
2021-05-02 00:58:11 +0300

    Redesign the UI and add the save/load buttons

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,16 +31,18 @@ namespace LimitlessDrawEngine
         {
             this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.selectShape = new System.Windows.Forms.CheckBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.selectShape = new System.Windows.Forms.CheckBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.shapes = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.panel3 = new System.Windows.Forms.Panel();
             this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.shapes = new System.Windows.Forms.Label();
             this.drawingCanvas = new System.Windows.Forms.Panel();
             this.sourcePanel = new System.Windows.Forms.Panel();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -48,36 +50,120 @@ namespace LimitlessDrawEngine
             this.button1 = new System.Windows.Forms.Button();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.drawingCanvas.SuspendLayout();
             this.sourcePanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.Gray;
-            this.panel1.Controls.Add(this.numericUpDown1);
-            this.panel1.Controls.Add(this.selectShape);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.BackColor = System.Drawing.Color.SkyBlue;
+            this.panel1.Controls.Add(this.loadButton);
+            this.panel1.Controls.Add(this.SaveButton);
             this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.selectShape);
+            this.panel1.Controls.Add(this.pictureBox3);
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Controls.Add(this.shapes);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.numericUpDown1);
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.comboBox3);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(10, 9);
+            this.panel1.Controls.Add(this.shapes);
+            this.panel1.Location = new System.Drawing.Point(707, -1);
             this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(751, 52);
+            this.panel1.Size = new System.Drawing.Size(64, 535);
             this.panel1.TabIndex = 0;
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(3, 57);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(61, 23);
+            this.loadButton.TabIndex = 13;
+            this.loadButton.Text = "load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(3, 28);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(59, 23);
+            this.SaveButton.TabIndex = 12;
+            this.SaveButton.Text = "save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(18, 311);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(28, 22);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // selectShape
+            // 
+            this.selectShape.AutoSize = true;
+            this.selectShape.Location = new System.Drawing.Point(23, 428);
+            this.selectShape.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.selectShape.Name = "selectShape";
+            this.selectShape.Size = new System.Drawing.Size(15, 14);
+            this.selectShape.TabIndex = 10;
+            this.selectShape.UseVisualStyleBackColor = true;
+            this.selectShape.CheckedChanged += new System.EventHandler(this.selectShape_CheckedChanged);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.White;
+            this.pictureBox3.Location = new System.Drawing.Point(19, 336);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(28, 22);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(18, 286);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(28, 22);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 396);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 30);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Select\r\nShape:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pen:";
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(60, 22);
+            this.numericUpDown1.Location = new System.Drawing.Point(8, 172);
             this.numericUpDown1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             48,
@@ -90,7 +176,7 @@ namespace LimitlessDrawEngine
             0,
             0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(67, 23);
+            this.numericUpDown1.Size = new System.Drawing.Size(46, 23);
             this.numericUpDown1.TabIndex = 11;
             this.numericUpDown1.Value = new decimal(new int[] {
             2,
@@ -99,75 +185,13 @@ namespace LimitlessDrawEngine
             0});
             this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
-            // selectShape
-            // 
-            this.selectShape.AutoSize = true;
-            this.selectShape.Location = new System.Drawing.Point(673, 26);
-            this.selectShape.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.selectShape.Name = "selectShape";
-            this.selectShape.Size = new System.Drawing.Size(15, 14);
-            this.selectShape.TabIndex = 10;
-            this.selectShape.UseVisualStyleBackColor = true;
-            this.selectShape.CheckedChanged += new System.EventHandler(this.selectShape_CheckedChanged);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(583, 23);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(76, 15);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Select Shape:";
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.BackColor = System.Drawing.Color.White;
-            this.pictureBox3.Location = new System.Drawing.Point(470, 21);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(28, 21);
-            this.pictureBox3.TabIndex = 8;
-            this.pictureBox3.TabStop = false;
-            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackColor = System.Drawing.Color.White;
-            this.pictureBox2.Location = new System.Drawing.Point(437, 21);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(28, 21);
-            this.pictureBox2.TabIndex = 7;
-            this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.Location = new System.Drawing.Point(403, 21);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(28, 21);
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
-            // 
-            // shapes
-            // 
-            this.shapes.AutoSize = true;
-            this.shapes.Location = new System.Drawing.Point(348, 23);
-            this.shapes.Name = "shapes";
-            this.shapes.Size = new System.Drawing.Size(47, 15);
-            this.shapes.TabIndex = 5;
-            this.shapes.Text = "Shapes:";
-            // 
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.Black;
-            this.panel3.Location = new System.Drawing.Point(133, 20);
+            this.panel3.Location = new System.Drawing.Point(17, 145);
             this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(25, 21);
+            this.panel3.Size = new System.Drawing.Size(27, 22);
             this.panel3.TabIndex = 4;
             this.panel3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel3_MouseClick);
             // 
@@ -176,34 +200,34 @@ namespace LimitlessDrawEngine
             this.comboBox3.FormattingEnabled = true;
             this.comboBox3.Items.AddRange(new object[] {
             "Solid",
-            "Dot",
             "Dash",
+            "Dot",
             "DashDot",
             "DashDotDot"});
-            this.comboBox3.Location = new System.Drawing.Point(164, 20);
+            this.comboBox3.Location = new System.Drawing.Point(4, 203);
             this.comboBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(70, 23);
+            this.comboBox3.Size = new System.Drawing.Size(55, 23);
             this.comboBox3.TabIndex = 3;
             this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
             // 
-            // label1
+            // shapes
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(30, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Pen:";
+            this.shapes.AutoSize = true;
+            this.shapes.Location = new System.Drawing.Point(8, 262);
+            this.shapes.Name = "shapes";
+            this.shapes.Size = new System.Drawing.Size(47, 15);
+            this.shapes.TabIndex = 5;
+            this.shapes.Text = "Shapes:";
             // 
             // drawingCanvas
             // 
             this.drawingCanvas.BackColor = System.Drawing.Color.White;
             this.drawingCanvas.Controls.Add(this.sourcePanel);
-            this.drawingCanvas.Location = new System.Drawing.Point(10, 76);
+            this.drawingCanvas.Location = new System.Drawing.Point(10, -1);
             this.drawingCanvas.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.drawingCanvas.Name = "drawingCanvas";
-            this.drawingCanvas.Size = new System.Drawing.Size(751, 458);
+            this.drawingCanvas.Size = new System.Drawing.Size(696, 535);
             this.drawingCanvas.TabIndex = 1;
             this.drawingCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.drawingCanvas_Paint);
             this.drawingCanvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.drawingCanvas_MouseClick);
@@ -217,13 +241,13 @@ namespace LimitlessDrawEngine
             this.sourcePanel.Controls.Add(this.textBox1);
             this.sourcePanel.Location = new System.Drawing.Point(0, 0);
             this.sourcePanel.Name = "sourcePanel";
-            this.sourcePanel.Size = new System.Drawing.Size(751, 457);
+            this.sourcePanel.Size = new System.Drawing.Size(696, 534);
             this.sourcePanel.TabIndex = 0;
             this.sourcePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.sourcePanel_Paint);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Location = new System.Drawing.Point(0, 1);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(540, 23);
             this.textBox1.TabIndex = 0;
@@ -269,10 +293,10 @@ namespace LimitlessDrawEngine
             this.Text = "Limitless Draw Engine 2000";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.drawingCanvas.ResumeLayout(false);
             this.sourcePanel.ResumeLayout(false);
             this.sourcePanel.PerformLayout();
@@ -299,6 +323,8 @@ namespace LimitlessDrawEngine
         private System.Windows.Forms.Panel sourcePanel;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button SaveButton;
     }
 }
 
