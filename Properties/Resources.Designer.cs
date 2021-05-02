a518301892f7cf094eceded69dd86fbb903cf4f8
Ahmed Alzubaidi <Ahmed.Alzubaidi@tuwaiq.edu.sa>
2021-05-02 05:27:20 +0300

    refactor: renamed project

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace LimitlessDrawEngine.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LimitlessDrawEngine.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DrawEngine.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
